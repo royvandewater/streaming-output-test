cdcc3702b0a109aa36b4dc739688726e50d24f2c
Roy van de Water <roy@circleci.com>
2021-10-18 09:59:55 -0700

    Fail if more than 6GB of ram

--- main.sh
+++ main.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+set -e -o pipefail
+
+get_mem_available() {
+	cat /proc/meminfo \
+	| grep MemTotal \
+	| awk '{print $2}'
+}
+
 main(){
-	return "$(($RANDOM % 2))"
+	local mem_available
+	mem_available="$(get_mem_available)"
+	return $(($mem_available > 6291456))
 }
 main "$@"
